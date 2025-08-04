ee44762455cb444924e93ddb50043ef1814b531f
Milika27 <liekie04@icloud.com>
2025-08-04 14:42:50 +0800

    change the menu options to switch case

---
+++ userMenu/src/main/java/UserMenuSwitch.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 30066318
+ */
+public class UserMenuSwitch {
+    Scanner in = new Scanner (System.in);
+    
+}
--- userMenu/src/main/java/com/mycompany/usermenu/UserMenu.java
+++ userMenu/src/main/java/com/mycompany/usermenu/UserMenu.java
@@ -24,25 +24,43 @@ public class UserMenu {
            
            int choice = in.nextInt ();
            
-           if (choice == 1)
-           {
-               System.out.println("You chose to borrow a book.");
-           }
-           else if (choice == 2)
-                   {
-                       System.out.println("You have chosen to return a book.");
-                   }
-           else if (choice == 3)
-           {
-               System.out.println("You have chosen to view all books.");
-           }
-           else if (choice == 4)
-           {
-               System.out.println("You chose to pay late fees.");
-           }
-           else
-           {
-               System.out.println("Invalid Choice.");
-           }
+//           if (choice == 1)
+//           {
+//               System.out.println("You chose to borrow a book.");
+//           }
+//           else if (choice == 2)
+//                   {
+//                       System.out.println("You have chosen to return a book.");
+//                   }
+//           else if (choice == 3)
+//           {
+//               System.out.println("You have chosen to view all books.");
+//           }
+//           else if (choice == 4)
+//           {
+//               System.out.println("You chose to pay late fees.");
+//           }
+//           else
+//           {
+//               System.out.println("Invalid Choice.");
+//           }
+        switch (choice) {
+            case 1:
+                System.out.println ("You have chosen to borrow a book.");
+                break;
+            case 2:
+                System.out.println ("You have chosen to return a book.");
+                break;
+            case 3:
+                System.out.println ("You have chosen to view all books.");
+                break;
+            case 4:
+                System.out.println ("You have chosen to pay late fees.");
+                break;
+            default:
+                System.out.println ("Invalid choice.");
+                
+         
+        }
     }
 }
