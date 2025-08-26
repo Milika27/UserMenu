71726f59de7b1c698487c9072c6eac80f0adacde
Milika27 <liekie04@icloud.com>
2025-08-26 09:09:25 +0800

    added a library, user and book class

---
+++ userMenu/src/main/java/Book.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.usermenu2;
+
+/**
+ *
+ * @author gelde
+ */
+public class Book {
+    public String title = "Throne of Glass";
+    public String author = "Sarah j. Maas";
+    public String publication = " 08/02/2012";
+    public int copies = 10;
+    public String dueDate = " 09/08/2025";
+}
